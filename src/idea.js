a7d4b2dbf3c575e485ac23b095daf5cf49bb24c6
Ian Holladay <holladayian@gmail.com>
2020-07-28 03:22:19 -0600

    fix typos

--- src/idea.js
+++ src/idea.js
@@ -6,10 +6,13 @@ class Idea {
     this.star = false;
   }
   saveToStorage() {
-
+    //4.1 setItem to localStorage,
+    //4.1 pass in the arguments of a good string name(-i.e. IdeaStored)
+    //4.1 and also JSON.stringify(ideaArray)
   }
   deleteFromStorage() {
-
+    //4.5 run our retrieve-from-storage function
+    //4.5 ourStringFromStorage.splice(this, 1)
   }
   updateIdea() {
     if (this.star) {
